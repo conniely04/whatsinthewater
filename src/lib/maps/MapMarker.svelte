c4ab27044740177f49fd9a0b0edfb9afe774f7fd
Jonathan Nguyen <jonathanguven@gmail.com>
2023-10-07 17:08:26 -0700

    new map files

--- src/lib/maps/MapMarker.svelte
+++ src/lib/maps/MapMarker.svelte
@@ -1,7 +1,6 @@
 <script>
     import { getContext } from 'svelte';
-    import mapbox from 'mapbox-gl';
-    const key = {};
+    import { mapbox, key } from './mapbox.js';
 
     const { getMap } = getContext(key);
     const map = getMap();
---
+++ src/lib/maps/mapbox.js
@@ -0,0 +1,8 @@
+import mapbox from 'mapbox-gl';
+
+// https://docs.mapbox.com/help/glossary/access-token/
+mapbox.accessToken = "pk.eyJ1IjoiY29ubmllbHkyMDA0IiwiYSI6ImNsbmdqeWVvZTB6NXoyc2xmZzRqY2RicXoifQ._nLR-VuK8TtSrRdInnWm3w";
+
+const key = {};
+
+export { mapbox, key };
